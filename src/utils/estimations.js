a97328e63bc0604c354e41c85bd1a7dc13700039
Nicolas Chevrier (t0194434) <nicolas.chevrier@thalesgroup.com>
2021-04-11 17:25:41 +0200

    Use targetHpMax and targetHpMin var.

--- src/utils/estimations.js
+++ src/utils/estimations.js
@@ -17,42 +17,42 @@ export const estimateHps = (target, hpState, damageReceived) => {
   console.log("target", target);
   console.log("hpState", hpState);
   console.log("damageReceived", damageReceived);
+
   let hpMax, hpMin;
+  let targetHpMax = target.hpMax ? target.hpMax : Infinity;
+  let targetHpMin= target.hpMin ? target.hpMin : -Infinity;
+
   if (target.hpState === hpState) {
     if (hpState === "green") {
       hpMin = Math.max(
-        3 * damageReceived + 1,
-        target.hpMin ? target.hpMin : -Infinity
+        3 * damageReceived + 1, targetHpMin
       );
     }
     if (hpState === "orange") {
       hpMin = Math.max(
-        (3 / 2) * damageReceived + 1,
-        target.hpMin ? target.hpMin : -Infinity
+        (3 / 2) * damageReceived + 1, targetHpMin
       );
     }
     if (hpState === "red") {
       hpMin = Math.max(
-        1 * damageReceived + 1,
-        target.hpMin ? target.hpMin : -Infinity
+        1 * damageReceived + 1, targetHpMin
       );
     }
   } else {
     if (hpState === "orange") {
       hpMax = Math.min(
-        3 * damageReceived,
-        target.hpMax ? target.hpMax : Infinity
+        3 * damageReceived, targetHpMax
       );
     }
 
     if (hpState === "red") {
       hpMax = Math.min(
-        (3 / 2) * damageReceived,
-        target.hpMax ? target.hpMax : Infinity
+        (3 / 2) * damageReceived, targetHpMax
       );
     }
+
     if (hpState === "dead") {
-      hpMax = Math.min(damageReceived, target.hpMax ? target.hpMax : Infinity);
+      hpMax = Math.min(damageReceived, targetHpMax);
     }
   }
 
