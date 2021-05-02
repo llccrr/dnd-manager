a6431009ced7dcb5fd6634216e3e02c0b76406ae
Loïc Renaud <loic1.renaud@hotmail.fr>
2021-05-03 00:07:20 +0200

    add avatar in the add monster

--- src/component/Form/Combobox.js
+++ src/component/Form/Combobox.js
@@ -1,54 +1,66 @@
 /* eslint-disable no-use-before-define */
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete, {
-  createFilterOptions,
-} from "@material-ui/lab/Autocomplete";
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
 
 const filter = createFilterOptions();
 
 export default function ComboBox({ list = [], onCreate, ...props }) {
-  const [value, setValue] = React.useState(null);
-  return (
-    <Autocomplete
-      value={value}
-      filterOptions={(options, params) => {
-        const filtered = filter(options, params);
+    const [value, setValue] = React.useState(null);
+    return (
+        <Autocomplete
+            value={value}
+            filterOptions={(options, params) => {
+                const filtered = filter(options, params);
 
-        // Suggest the creation of a new value
-        if (params.inputValue !== "") {
-          // onCreate();
-          filtered.push({
-            inputValue: params.inputValue,
-            title: `Add "${params.inputValue}"`,
-          });
-        }
+                // Suggest the creation of a new value
+                if (params.inputValue !== '') {
+                    // onCreate();
+                    filtered.push({
+                        inputValue: params.inputValue,
+                        title: `Add "${params.inputValue}"`,
+                    });
+                }
 
-        return filtered;
-      }}
-      selectOnFocus
-      clearOnBlur
-      handleHomeEndKeys
-      options={list}
-      getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
-        if (typeof option === "string") {
-          return option;
-        }
-        // Add "xxx" option created dynamically
-        if (option.inputValue) {
-          return option.inputValue;
-        }
-        // Regular option
-        return option.title;
-      }}
-      renderOption={(option) => option.title}
-      style={{ width: 300 }}
-      freeSolo
-      renderInput={(params) => (
-        <TextField {...params} label="Monsters" variant="outlined" />
-      )}
-      {...props}
-    />
-  );
+                return filtered;
+            }}
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            options={list}
+            getOptionLabel={(option) => {
+                // Value selected with enter, right from the input
+                if (typeof option === 'string') {
+                    return option;
+                }
+                // Add "xxx" option created dynamically
+                if (option.inputValue) {
+                    return option.inputValue;
+                }
+                // Regular option
+                return option.title;
+            }}
+            renderOption={(option) => (
+                <Row>
+                    <MonsterAvatar src={option.avatarUrl} /> {option.title}
+                </Row>
+            )}
+            style={{ width: 300 }}
+            freeSolo
+            renderInput={(params) => <TextField {...params} label="Monsters" variant="outlined" />}
+            {...props}
+        />
+    );
 }
+
+const Row = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const MonsterAvatar = styled(Avatar)`
+    margin: 0 10px;
+    width: 40px;
+    height: 40px;
+`;
--- src/component/GameSheet.js
+++ src/component/GameSheet.js
@@ -1,211 +1,199 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { EventsTable } from "./EventsTable";
-import ComboBox from "./Form/Combobox";
-import { CurrentBoard } from "./CurrentBoard";
-import { Players } from "./Players";
-import { EventBuilder } from "./EventBuilder";
-import { listenData, pushData, writeData } from "../firebase/database";
-import { Button } from "@material-ui/core";
-import { estimateAc, estimateHps } from "../utils/estimations";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { EventsTable } from './EventsTable';
+import ComboBox from './Form/Combobox';
+import { CurrentBoard } from './CurrentBoard';
+import { Players } from './Players';
+import { EventBuilder } from './EventBuilder';
+import { listenData, pushData, writeData } from '../firebase/database';
+import { Button } from '@material-ui/core';
+import { estimateAc, estimateHps } from '../utils/estimations';
 
 export const GameSheets = () => {
-  const [monsters, setMonsters] = useState({});
-  const [currentBoard, setCurrentBoard] = useState({});
-  const [players, setPlayers] = useState({});
-  const [actor, setActor] = useState(null);
-  const [target, setTarget] = useState({});
+    const [monsters, setMonsters] = useState({});
+    const [currentBoard, setCurrentBoard] = useState({});
+    const [players, setPlayers] = useState({});
+    const [actor, setActor] = useState(null);
+    const [target, setTarget] = useState({});
 
-  useEffect(() => {
-    listenData("players", (players) => {
-      setPlayers(players);
-    });
-    listenData("monsters", (monsters) => {
-      setMonsters(monsters);
-    });
-    listenData("currentBoard", (board) => {
-      console.log("currentBoard", currentBoard);
-      setCurrentBoard(board);
-    });
-  }, []);
+    useEffect(() => {
+        listenData('players', (players) => {
+            setPlayers(players);
+        });
+        listenData('monsters', (monsters) => {
+            setMonsters(monsters);
+        });
+        listenData('currentBoard', (board) => {
+            console.log('currentBoard', currentBoard);
+            setCurrentBoard(board);
+        });
+    }, []);
 
-  const instanceNewGame = () => {
-    const game = { gameId: new Date().getTime(), monsters: [] };
-    writeData("currentBoard/", game);
-  };
+    const instanceNewGame = () => {
+        const game = { gameId: new Date().getTime(), monsters: [] };
+        writeData('currentBoard/', game);
+    };
 
-  const saveAndEnd = () => {
-    console.log("saving..");
-    console.log("currBoard", currentBoard);
-    pushData("games", currentBoard);
-  };
+    const saveAndEnd = () => {
+        console.log('saving..');
+        console.log('currBoard', currentBoard);
+        pushData('games', currentBoard);
+    };
 
-  const addMonsterToGame = (id) => {
-    writeData(`currentBoard/monsters/${currentBoard.monsters?.length || 0}`, {
-      monsterId: id,
-      damageReceived: 0
-    });
-  };
+    const addMonsterToGame = (id) => {
+        writeData(`currentBoard/monsters/${currentBoard.monsters?.length || 0}`, {
+            monsterId: id,
+            damageReceived: 0,
+        });
+    };
 
-  const onEventValidated = (eventObj) => () => {
-    // Add the event
-    console.log("ici");
-    const newTarget = target;
-    Object.keys(target).forEach(
-      (key) => newTarget[key] === undefined && delete newTarget[key]
+    const onEventValidated = (eventObj) => () => {
+        // Add the event
+        console.log('ici');
+        const newTarget = target;
+        Object.keys(target).forEach((key) => newTarget[key] === undefined && delete newTarget[key]);
+        console.log(newTarget);
+
+        pushData(`currentBoard/events/`, {
+            ...eventObj,
+            target: newTarget,
+            actor,
+            creationDate: new Date().getTime(),
+        });
+        const targetKey = currentBoard.monsters.findIndex((it) => it.gameId === target.gameId);
+
+        console.log('currentBoard.monsters', currentBoard.monsters);
+        console.log('target', target);
+
+        if (targetKey === -1) {
+            console.log('didnt find the minster');
+            return;
+        }
+        // Update traget into database
+        if (eventObj.type === 'attack') {
+            if (eventObj.touchScore) {
+                const ac = estimateAc(target, eventObj.touchScore, eventObj.value);
+                console.log('ac', ac);
+                const [key, value] = Object.entries(ac)[0];
+                // update CA
+                writeData(`monsters/${target.monsterId}/${key}`, value);
+            }
+            if (eventObj.value) {
+                console.log('target.gameId', target.gameId);
+                console.log('currentBoard.monsters', currentBoard.monsters);
+
+                const newDamageReceived = parseInt(target.damageReceived || 0) + parseInt(eventObj.value || 0);
+                writeData(`currentBoard/monsters/${targetKey}/damageReceived`, newDamageReceived);
+                const newHps = estimateHps(target, eventObj.hpState, newDamageReceived);
+
+                writeData(`currentBoard/monsters/${targetKey}/hpState`, eventObj.hpState);
+                console.log('newHps', newHps);
+
+                if (newHps.hpMax) {
+                    writeData(`monsters/${target.monsterId}/hpMax`, newHps.hpMax);
+                }
+                if (newHps.hpMin) {
+                    writeData(`monsters/${target.monsterId}/hpMin`, newHps.hpMin);
+                }
+            }
+
+            // update Hp
+        }
+    };
+    const handleComboboxChange = (e, r) => {
+        const { inputValue, title, id } = r;
+        if (inputValue) {
+            // create monster
+            createNewMonster(inputValue);
+            return;
+        }
+        addMonsterToGame(id);
+    };
+
+    const createNewMonster = (name) => {
+        const date = new Date().getTime();
+        writeData(`/monsters/${date}`, { monsterId: date, name, hpState: 'green' });
+    };
+
+    const updateGameId = (index) => (e) => {
+        e.preventDefault();
+        writeData(`currentBoard/monsters/${index}/gameId`, e.target.value);
+    };
+
+    const onMonsterAvatarChange = (monsterId) => (e) => {
+        e.preventDefault();
+        // console.log('value', e.target.value)
+        //   console.log('monsterId', monsterId);
+        writeData(`monsters/${monsterId}/avatarUrl`, e.target.value);
+    };
+
+    const setActorOrTargets = (entity) => {
+        if (!actor) {
+            setActor(entity);
+        } else {
+            // setTargets((targets) => [...targets, entity]);
+            setTarget(entity);
+        }
+    };
+
+    const onSelectedPlayerChange = (monster) => () => {
+        setActorOrTargets(monster);
+    };
+
+    const onPlayerClick = (player) => () => {
+        setActorOrTargets(player);
+    };
+    const populateBoard = {
+        ...currentBoard,
+        monsters: currentBoard?.monsters?.map((boardMonster) => ({
+            ...monsters[boardMonster.monsterId],
+            ...boardMonster,
+        })),
+    };
+    const gameEvents = Object.values(currentBoard.events || {}).sort((a, b) => b.creationDate - a.creationDate);
+
+    return (
+        <Container>
+            <Button variant="default" onClick={instanceNewGame}>
+                New game
+            </Button>
+            <Button onClick={saveAndEnd}>Save and end</Button>
+
+            <ComboBox
+                onCreate={() => {
+                    console.log('on create');
+                }}
+                onChange={handleComboboxChange}
+                onInputChange={() => {
+                    // console.log("on input change");
+                }}
+                list={
+                    monsters
+                        ? Object.values(monsters).map((monster) => ({
+                              title: monster.name,
+                              id: monster.monsterId,
+                              avatarUrl: monster.avatarUrl,
+                          }))
+                        : []
+                }
+            />
+            <Players players={players} onPlayerClick={onPlayerClick} />
+            <CurrentBoard
+                board={populateBoard}
+                updateGameId={updateGameId}
+                onSelectedChange={onSelectedPlayerChange}
+                onMonsterAvatarChange={onMonsterAvatarChange}
+            />
+            <EventBuilder
+                actor={actor}
+                target={target}
+                setActor={setActor}
+                setTarget={setTarget}
+                onValidate={onEventValidated}
+            />
+            <EventsTable events={gameEvents} />
+        </Container>
     );
-    console.log(newTarget);
-
-    pushData(`currentBoard/events/`, {
-      ...eventObj,
-      target: newTarget,
-      actor,
-      creationDate: new Date().getTime(),
-    });
-    const targetKey = currentBoard.monsters.findIndex(
-      (it) => it.gameId === target.gameId
-    );
-
-    console.log("currentBoard.monsters", currentBoard.monsters);
-    console.log("target", target);
-
-    if (targetKey === -1) {
-      console.log("didnt find the minster");
-      return;
-    }
-    // Update traget into database
-    if (eventObj.type === "attack") {
-      if (eventObj.touchScore) {
-        const ac = estimateAc(target, eventObj.touchScore, eventObj.value);
-        console.log("ac", ac);
-        const [key, value] = Object.entries(ac)[0];
-        // update CA
-        writeData(`monsters/${target.monsterId}/${key}`, value);
-      }
-      if (eventObj.value) {
-        console.log("target.gameId", target.gameId);
-        console.log("currentBoard.monsters", currentBoard.monsters);
-
-        const newDamageReceived =
-          parseInt(target.damageReceived || 0) + parseInt(eventObj.value || 0);
-        writeData(
-          `currentBoard/monsters/${targetKey}/damageReceived`,
-          newDamageReceived
-        );
-        const newHps = estimateHps(target, eventObj.hpState, newDamageReceived);
-
-        writeData(
-          `currentBoard/monsters/${targetKey}/hpState`,
-          eventObj.hpState
-        );
-        console.log("newHps", newHps);
-
-        if (newHps.hpMax) {
-          writeData(`monsters/${target.monsterId}/hpMax`, newHps.hpMax);
-        }
-        if (newHps.hpMin) {
-          writeData(`monsters/${target.monsterId}/hpMin`, newHps.hpMin);
-        }
-      }
-
-      // update Hp
-    }
-  };
-  const handleComboboxChange = (e, r) => {
-    const { inputValue, title, id } = r;
-    if (inputValue) {
-      // create monster
-      createNewMonster(inputValue);
-      return;
-    }
-    addMonsterToGame(id);
-  };
-
-  const createNewMonster = (name) => {
-    const date = new Date().getTime();
-    writeData(`/monsters/${date}`, { monsterId: date, name, hpState: "green" });
-  };
-
-  const updateGameId = (index) => (e) => {
-    e.preventDefault();
-    writeData(`currentBoard/monsters/${index}/gameId`, e.target.value);
-  };
-
-  const onMonsterAvatarChange = (monsterId) => (e) => {
-    e.preventDefault();
-    // console.log('value', e.target.value)
-    //   console.log('monsterId', monsterId);
-      writeData(`monsters/${monsterId}/avatarUrl`, e.target.value);
-  };
-
-  const setActorOrTargets = (entity) => {
-    if (!actor) {
-      setActor(entity);
-    } else {
-      // setTargets((targets) => [...targets, entity]);
-      setTarget(entity);
-    }
-  };
-
-  const onSelectedPlayerChange = (monster) => () => {
-    setActorOrTargets(monster);
-  };
-
-  const onPlayerClick = (player) => () => {
-    setActorOrTargets(player);
-  };
-  const populateBoard = {
-    ...currentBoard,
-    monsters: currentBoard?.monsters?.map((boardMonster) => ({
-      ...monsters[boardMonster.monsterId],
-      ...boardMonster,
-    })),
-  };
-  const gameEvents = Object.values(currentBoard.events || {}).sort(
-    (a, b) => b.creationDate - a.creationDate
-  );
-
-  return (
-    <Container>
-      <Button variant="default" onClick={instanceNewGame}>
-        New game
-      </Button>
-      <Button onClick={saveAndEnd}>Save and end</Button>
-
-      <ComboBox
-        onCreate={() => {
-          console.log("on create");
-        }}
-        onChange={handleComboboxChange}
-        onInputChange={() => {
-          // console.log("on input change");
-        }}
-        list={
-          monsters
-            ? Object.values(monsters).map((monster) => ({
-                title: monster.name,
-                id: monster.monsterId,
-              }))
-            : []
-        }
-      />
-      <Players players={players} onPlayerClick={onPlayerClick} />
-      <CurrentBoard
-        board={populateBoard}
-        updateGameId={updateGameId}
-        onSelectedChange={onSelectedPlayerChange}
-        onMonsterAvatarChange={onMonsterAvatarChange}
-      />
-      <EventBuilder
-        actor={actor}
-        target={target}
-        setActor={setActor}
-        setTarget={setTarget}
-        onValidate={onEventValidated}
-      />
-      <EventsTable events={gameEvents} />
-    </Container>
-  );
 };
 
 // const NewGameButton = styled.button`
@@ -213,5 +201,5 @@ export const GameSheets = () => {
 // `;
 
 const Container = styled.div`
-  margin: 0 15px;
+    margin: 0 15px;
 `;
